e9018b7a1a01b07a610be4af1e67abe4dd85e384
Collin Sherman <collin@Collins-MacBook-Air.local>
2022-01-02 14:50:35 -0700

    logic on client side

--- client.js
+++ client.js
@@ -7,8 +7,27 @@ const client = net.createConnection({ port: 6000 }, () => {
     console.log('connected')
     console.log()
 })
-process.stdin.on('data', (data) => { 
-    client.write(data)
+process.stdin.on('data', (data) => {
+    let dataArray = data.split(" ")
+    
+    if(dataArray[0].toString().trimEnd() == '/w'){
+        console.log("whisper")
+        client.write(dataArray.toString())
+        client.write("whisper")
+    }
+    else if(dataArray[0].toString().trimEnd() == '/username'){
+        console.log("username")
+        client.write(dataArray)
+    }
+    else if(dataArray[0].toString().trimEnd() == '/kick'){
+        console.log("kick")
+        client.write(dataArray)
+    }
+    else if(dataArray[0].toString().trimEnd() == '/clientlist'){
+        console.log("clientlist")
+        client.write(dataArray)
+    }
+    else{client.write(data)}
 })
 client.on('data', (data) => {
     console.log(data.toString());
